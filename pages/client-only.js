588c45d7b9a05f07fa38ba43d5d6b27d693854e0
ConradoLuiz <conradol.vargas@gmail.com>
2020-06-11 16:31:42 -0300

    withApollo not getDataFromTree

--- pages/client-only.js
+++ pages/client-only.js
@@ -2,7 +2,8 @@ import App from '../components/App'
 import InfoBox from '../components/InfoBox'
 import Header from '../components/Header'
 import Submit from '../components/Submit'
-import PostList from '../components/PostList'
+import PostList from '../components/PostList';
+import withApollo from "../lib/withApollo";
 
 const ClientOnlyPage = (props) => (
   <App>
@@ -18,4 +19,6 @@ const ClientOnlyPage = (props) => (
   </App>
 )
 
-export default ClientOnlyPage
+// Here I use ssr: false since this page doesnt care about ssr
+// and only fetches data on client side
+export default withApollo({ ssr: false })(ClientOnlyPage);
--- pages/index.js
+++ pages/index.js
@@ -7,27 +7,38 @@ import PostList, {
   allPostsQueryVars,
 } from '../components/PostList'
 import { initializeApollo, useApollo } from '../lib/apolloClient'
+import NextApp from 'next/app';
+import withApollo from "../lib/withApollo";
 
-const IndexPage = ({props: {initialApolloState}}) => {
-  const apolloClient = useApollo(initialApolloState);
-  
+
+const IndexPage = () => {
   return (
     <App>
       <Header />
       <InfoBox>ℹ️ This page shows how to use SSG with Apollo.</InfoBox>
       <Submit />
-      <PostList client={apolloClient}/>
+      <PostList />
     </App>
 )}
 
-
+// Since getInitialProps runs on the server, on first load
+// I query the data the page is going to need and runs smooth 
+// with SSR. When the user navigates to this page through client 
+// side routing, getInitialProps runs on the browser and attemps to 
+// make this query. BUT if the data that the page needs is already 
+// cached, I dont need to make the query at all, instead I just get
+// the data from the apollo store
 IndexPage.getInitialProps = async (context) => {
-  const apolloClient = initializeApollo()
+
+  const apolloClient = context.apolloClient;
   
+  // This just makes one query, but if the page needs multiple queries
+  // we can make use of apollo fragments
+  // But i havent tested that out yet
   await apolloClient.query({
     query: ALL_POSTS_QUERY,
     variables: allPostsQueryVars,
-  })
+  });
 
   return {
     props: {
@@ -36,20 +47,9 @@ IndexPage.getInitialProps = async (context) => {
   }
 }
 
-// export async function getServerSideProps(obj) {
-//   const apolloClient = initializeApollo()
-//   console.log(obj);
-  
-//   await apolloClient.query({
-//     query: ALL_POSTS_QUERY,
-//     variables: allPostsQueryVars
-//   });
 
-//   return {
-//     props : {
-//       initialApolloState: apolloClient.cache.extract()
-//     }
-//   }
-// }
-
-export default IndexPage
+// Here I use <ssr: false> to not let the WithApollo library 
+// invoke getDataFromTree since it hinders perfomance.
+// Instead I want to make the queries myself. And I do so in the
+// function above, getInitialProps
+export default withApollo({ ssr: false })(IndexPage);
